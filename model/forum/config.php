7e63c9eac28d9dae35d8b574ec245417daa3ad10
aysonsteven <aysonsteven@gmail.com>
2017-02-16 22:04:44 +0800

    fixing error

--- model/forum/config.php
+++ model/forum/config.php
@@ -6,15 +6,9 @@ namespace model\forum;
 class Config extends Forum {
     public function __construct()
     {
-
         parent::__construct();
-
         $this->setTable('forum_config');
-
-
     }
-
-
     /**
      *
      *
@@ -23,96 +17,41 @@ class Config extends Forum {
      *
      */
     public function create() {
-
-
-        if( empty( in('id') ) ) return error( ERROR_USER_ID_EMPTY );
-        if( empty( in('session_id') ) ) return error( ERROR_SESSION_ID_EMPTY );
-
-
-        $user = user()->load_by_session_id( in('session_id') );
-        if ( empty($user) ) return error( ERROR_USER_NOT_EXIST );
-        if( $user['id'] <>  'admin' ) return error( ERROR_PERMISSION_ADMIN );
-
         $data = [];
         $data['id'] = in('id');
         $data['name'] = in('name');
         $data['description'] = in('description');
-
-
         $config = $this->get( in('id') );
         if ( $config ) return error( ERROR_FORUM_CONFIG_EXIST );
-
         $forum_idx = $this->insert( $data );
-
         if ( $forum_idx <= 0 ) error( $forum_idx );
-        else success( ['idx'=>$forum_idx, 'id'=>$data['id']] );
-
+        else success( ['idx'=>$forum_idx] );
     }
-
     /**
      * @return mixed
      */
     public function edit() {
-        if( empty( in('idx') ) ) return error( ERROR_IDX_EMPTY );
-        if( empty( in('session_id' ) ) ) return( ERROR_SESSION_ID_EMPTY );
-        if( empty( in('id') ) ) return error( ERROR_FORUM_ID_EMPTY );
-
-        $user = user()->load_by_session_id( in('session_id') );
-        if ( empty($user) ) return error( ERROR_USER_NOT_EXIST );
-        if( $user['id'] <>  'admin' ) return error( ERROR_PERMISSION_ADMIN );
-
         $data = [];
         $data['idx'] = in('idx');
         $data['id'] = in('id');
         $data['name'] = in('name');
         $data['description'] = in('description');
-
         $config_data = $this->load($data['idx']);
         if( ! $config_data ) return error( ERROR_FORUM_CONFIG_NOT_EXIST );
-
-
         unset( $data['idx'] ); //
-
         $no = $this->update($data);
         if ( $no == 0 ) return error( ERROR_UNKNOWN ); // strange error. this error should not happened here.
-
         success( ['forum_data' => $this->load( in('idx') ) ] );
-
     }
-
     public function delete( $compatibility_arg1 = null ) {
-        if( empty( in('session_id') ) ) return error( ERROR_SESSION_ID_EMPTY );
-
-        $user = user()->load_by_session_id( in('session_id') );
-        if ( empty($user) ) return error( ERROR_USER_NOT_EXIST );
-        if( $user['id'] <>  'admin' ) return error( ERROR_PERMISSION_ADMIN );
-
         $idx = in('idx');
         if ( empty($idx) ) $idx = "id='".in('id')."'"; // if it is not string, it is id.
-
-
         // di($idx);
-
         $config = $this->load( $idx );
         if( !$config ) error( ERROR_FORUM_CONFIG_NOT_EXIST );
-
         //$condition = "idx = $idx";
         //$this->delete( $condition );
-
         $this->destroy();
-
         success();
     }
-
-
-    public function getConfig() {
-        if( empty( in( 'session_id') ) ) return error( ERROR_SESSION_ID_EMPTY );
-        if( empty( in('id') ) ) return error( ERROR_FORUM_ID_EMPTY );
-
-        $config = $this->get( in('id') );
-        if( empty( $config ) ) error( ERROR_FORUM_CONFIG_NOT_EXIST );
-
-        success( ['forum_data' => $config]);
-    }
-
 }
--- model/test/all.php
+++ model/test/all.php
@@ -348,6 +348,7 @@ To install access to ?mc=system.install
 
 
 
+
         $re = $this->ex( "\\model\\forum\\Config::create", $params );
         if ( $re['code'] == ERROR_FORUM_CONFIG_EXIST ) { // if exists,
             $re = $this->ex( "\\model\\forum\\Config::delete", $params ); // delete
@@ -355,22 +356,14 @@ To install access to ?mc=system.install
             $re = $this->ex( "\\model\\forum\\Config::create", $params ); // create
         }
         test( $re['code'] == 0, "Creating forum config - $params[id], " . error_string( $re ));
-
         $forum_config_idx = $re['data']['idx'];
-
         $re = $this->ex( "\\model\\forum\\Config::create", $params );
         test( $re['code'] == ERROR_FORUM_CONFIG_EXIST, "Forum config already exist: $params[id]. " . error_string($re) );
-
-
         $editconfig = ['idx'=>$forum_config_idx, 'id'=>'edit-forum', 'name'=>'edit ForumConfig'];
-
         $re = $this->ex( "\\model\\forum\\Config::edit", $editconfig);
         test( $re['code'] == 0, "Updating Forum config - $params[id]");
-
         $re = $this->ex( "\\model\\forum\\Config::delete", $editconfig );
         test( $re['code'] == 0, "Deleting forum config - $editconfig[id]");
-
-
         $re = $this->ex( "\\model\\forum\\Config::delete", $editconfig );
         test( $re['code'] == ERROR_FORUM_CONFIG_NOT_EXIST, "Forum already deleted - $editconfig[id]");
 
@@ -382,22 +375,23 @@ To install access to ?mc=system.install
 
 
         private function createForumData( $params ) {
-        $data = [
-            'id'=> 'user-id-' . time(),
-            'name'=> 'user-name-' . time()
-        ];
-        $session_id = $this->createUser( $data );
-        $params['session_id'] = $session_id;
-        $re = $this->ex( "\\model\\forum\\Data::create", $params );
-        test( $re['code'] == 0, "Creating forum config - $params[title]. " . error_string( $re ));
-        $forum_data_idx = $re['data']['forum_data'];
-        $editdata = ['idx'=>$forum_data_idx, 'title'=>'edit-data', 'content' => 'edit ForumData'];
-        $re = $this->ex( "\\model\\forum\\Data::edit", $editdata );
-        test( $re['code'] == 0, "Updating Forum Data - $editdata[title]");
-        $re = $this->ex( "\\model\\forum\\Data::delete", $editdata );
-        test( $re['code'] == 0 , "Deleting forum data - $editdata[title]");
-        $re = $this->ex( "\\model\\forum\\Data::delete", $editdata );
-        test( $re['code'] == ERROR_FORUM_DATA_NOT_EXIST , "Forum Data Already deleted - $editdata[title]");
+
+            $data = [
+                'id'=> 'user-id-' . time(),
+                'name'=> 'user-name-' . time()
+            ];
+            $session_id = $this->createUser( $data );
+            $params['session_id'] = $session_id;
+            $re = $this->ex( "\\model\\forum\\Data::create", $params );
+            test( $re['code'] == 0, "Creating forum config - $params[title]. " . error_string( $re ));
+            $forum_data_idx = $re['data']['forum_data'];
+            $editdata = ['idx'=>$forum_data_idx, 'title'=>'edit-data', 'content' => 'edit ForumData'];
+            $re = $this->ex( "\\model\\forum\\Data::edit", $editdata );
+            test( $re['code'] == 0, "Updating Forum Data - $editdata[title]");
+            $re = $this->ex( "\\model\\forum\\Data::delete", $editdata );
+            test( $re['code'] == 0 , "Deleting forum data - $editdata[title]");
+            $re = $this->ex( "\\model\\forum\\Data::delete", $editdata );
+            test( $re['code'] == ERROR_FORUM_DATA_NOT_EXIST , "Forum Data Already deleted - $editdata[title]");
     }
 
     private function testUserSearch()

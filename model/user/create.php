9eedfb1953676a4b42aaba4806be7a20ad98f003
JaeHo Song <thruthesky@JaeHos-MacBook-Pro.local>
2017-02-25 14:34:16 +0800

    updating user reg

--- model/user/create.php
+++ model/user/create.php
@@ -15,8 +15,13 @@ class Create extends User {
         if( strlen( in('id') ) >64 ) return error( ERROR_ID_IS_TOO_LONG );
         if ( empty( in('password') ) ) return error( ERROR_PASSWORD_EMPTY );
         if( strlen( in('password') ) > 255 ) return error( ERROR_PASSWORD_TOO_LONG );
-        if ( is_string( in('mobile') ) ) return error( ERROR_MOBILE_NOT_NUMERIC );
-        if( strlen( in('mobile') ) > 32 ) return error( ERROR_MOBILE_TOO_LONG );
+
+
+        $mobile = in('mobile');
+        if ( $mobile ) {
+            if ( ! is_numeric( $mobile ) ) return error( ERROR_MOBILE_NOT_NUMERIC );
+            if( strlen( $mobile ) > 32 ) return error( ERROR_MOBILE_TOO_LONG );
+        }
 
 
 
@@ -29,7 +34,7 @@ class Create extends User {
         $data['name'] = in('name');
         $data['birth_day'] = in('birth_day', 0);
         $data['gender'] = in('gender');
-        $data['mobile'] = in('mobile');
+        $data['mobile'] = $mobile;
         $data['landline'] = in('landline');
         $data['country'] = in('country');
         $data['city'] = in('city');
